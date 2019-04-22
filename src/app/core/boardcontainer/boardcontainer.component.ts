1fcddf30a0e6fdb7d1ab3392e273a7973cd1aac6
Nertil Poci <nertilpoci@hotmail.com>
2019-04-22 03:48:10 +0200

    update

--- src/app/core/boardcontainer/boardcontainer.component.ts
+++ src/app/core/boardcontainer/boardcontainer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation, Output, EventEmitter, Input, OnDe
 import { ResizeService } from '../shared/services/resize.service';
 import { Subscription } from 'rxjs';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { ClrSelectedState } from '@clr/angular';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'app-board-container',
@@ -47,5 +48,66 @@ export class BoardElementContainerComponent implements OnInit, OnDestroy {
       this.mobileQuery.removeListener(this._mobileQueryListener);
 
     }
+    root = [
+      {
+          name: "src",
+          selected: ClrSelectedState.INDETERMINATE,
+          files: [
+              {
+                  name: "app",
+                  selected: ClrSelectedState.INDETERMINATE,
+                  files: [
+                      {
+                          name: "app.component.html",
+                          selected: ClrSelectedState.UNSELECTED
+                      },
+                      {
+                          name: "app.component.ts",
+                          selected: ClrSelectedState.UNSELECTED
+                      },
+                      {
+                          name: "app.module.ts",
+                          selected: ClrSelectedState.SELECTED
+                      },
+                      {
+                          name: "app.routing.ts",
+                          selected: ClrSelectedState.UNSELECTED
+                      }
+                  ]
+              },
+              {
+                  name: "environments",
+                  selected: ClrSelectedState.SELECTED,
+                  files: [
+                      {
+                          name: "environments.prod.ts",
+                          selected: ClrSelectedState.SELECTED
+                      },
+                      {
+                          name: "environment.ts",
+                          selected: ClrSelectedState.SELECTED
+                      }
+                  ]
+              },
+              {
+                  name: "index.html",
+                  selected: ClrSelectedState.UNSELECTED,
+              },
+              {
+                  name: "main.ts",
+                  selected: ClrSelectedState.UNSELECTED,
+              }
+          ]
+      },
+      {
+          name: "package.json",
+          selected: ClrSelectedState.UNSELECTED
+      },
+      {
+          name: "tsconfig.json",
+          selected: ClrSelectedState.UNSELECTED
+      }
+  ];
 
+  getChildren = (folder) => folder.files;
 }
